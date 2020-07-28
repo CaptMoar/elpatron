b72325771a5363be0d3579935bc2171268352fd8
Jose Velasquez Riquelme <jvelasquez@bst.cl>
2020-07-28 18:45:11 -0400

    nuevos métodos para búsquedas por Rut, Fecha y Estado

--- handler.js
+++ handler.js
@@ -41,11 +41,89 @@ app.post('/resumen', (req, res) => {
   });
 });
 
+app.post('/resumen/getByRut', (req, res) => {
+  var params = {
+    TableName: "ssff-informe-resumen",
+    FilterExpression: 'rut = :rut',
+    ExpressionAttributeValues: {
+      //":rut": {"S": "012019183-7"}     
+      ":rut": {"S": JSON.parse(req.body.toString('utf8')).rut} 
+    }  
+  }
+  docClient.scan(params, async (err, result) => {
+    if (err) {
+      console.log(`[ERR] ${err}`)
+    } else {
+      const { items } = await result
+      res.json({
+        statusCode: 200,
+        headers: {
+          'Access-Control-Allow-Origin': '*',
+          'Access-Control-Allow-Credentials': true,
+        }, 
+        body: result
+      });
+    }
+  });
+});
+
+app.post('/resumen/getByFecha', (req, res) => {
+  var params = {
+    TableName: "ssff-informe-resumen",
+    FilterExpression: 'fecha = :fecha',
+    ExpressionAttributeValues: {
+      //":fecha": {"S": "2020-07-23"}     
+      ":fecha": {"S": JSON.parse(req.body.toString('utf8')).fecha} 
+    }  
+  }
+  docClient.scan(params, async (err, result) => {
+    if (err) {
+      console.log(`[ERR] ${err}`)
+    } else {
+      const { items } = await result
+      res.json({
+        statusCode: 200,
+        headers: {
+          'Access-Control-Allow-Origin': '*',
+          'Access-Control-Allow-Credentials': true,
+        }, 
+        body: result
+      });
+    }
+  });
+});
+
+app.post('/resumen/getByEstado', (req, res) => {
+  var params = {
+    TableName: "ssff-informe-resumen",
+    FilterExpression: 'estado = :estado',
+    ExpressionAttributeValues: {
+      //":estado": {"S": "EN PROCESO"}     
+      ":estado": {"S": JSON.parse(req.body.toString('utf8')).estado} 
+    }  
+  }
+  docClient.scan(params, async (err, result) => {
+    if (err) {
+      console.log(`[ERR] ${err}`)
+    } else {
+      const { items } = await result
+      res.json({
+        statusCode: 200,
+        headers: {
+          'Access-Control-Allow-Origin': '*',
+          'Access-Control-Allow-Credentials': true,
+        }, 
+        body: result
+      });
+    }
+  });
+});
+
 app.post('/auditoria/getById', (req, res) => {
   var params = {
     KeyConditionExpression: 'id_audio = :id_audio',
-    ExpressionAttributeValues: {
-        ':id_audio': {'S': req.body.id_audio}
+    ExpressionAttributeValues: {      
+      ':id_audio': {'S': JSON.parse(req.body.toString('utf8')).id_audio}
     },
     TableName: "ssff-informe" 
   }  
@@ -64,7 +142,6 @@ app.post('/auditoria/getById', (req, res) => {
       });
     }
   });
-
 });
 
 app.post('/informe', (req, res) => {
